7b1e2250b5ff63049321ad2fdd14891f616160dd
MisterioRee <Rehan@awj.ae>
2019-07-16 06:42:51 +0400

    Completing the Test Case

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php 
 
-//require_once './src/ProfessorHashInterface.php';
+require_once './src/ProfessorHashInterface.php';
 require_once './src/ProfessorHash.php';
 
 $hashObj = new  \ProfessorHash\ProfessorHash();
 
+$randomString = $hashObj->randomString();
+$hashedString = $hashObj->makeHash( $randomString );
+
+
+print_r( "Random String: " . $randomString .' <br>'  );
+print_r( "Random Hash: " . $hashedString  );
